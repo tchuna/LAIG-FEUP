81b51ca701a55e7d11d5c5e6efea633d3779ade0
Arthur26041996 <arthurmatta@hotmail.com>
2018-11-17 19:27:27 +0000

    LinearAnimation: updateMovementDirection

--- TP1/tp1/myAnimations/LinearAnimation.js
+++ TP1/tp1/myAnimations/LinearAnimation.js
@@ -15,18 +15,12 @@ class LinearAnimation extends Animation{
     this.totalDistance = 0;
     // Distance between two conscutive points
     this.distancePerSegment = [];
+    // Current position
+    this.currentPosition = 0;
     // Time between two consecutive points
     this.timePerSegment = [];
     // Index of current line segment
     this.index = 0;
-    // Direction of the movement between two consecutive points
-    this.anglePerSegment = [];
-
-    this.initValues();
-
-  }
-
-  initValues(){
     // Calculate the distance between conscutive control points, and the total distance
     var distance;
     for (let i = 0; i < this.controlPoints.length-1; i++) {
@@ -35,13 +29,37 @@ class LinearAnimation extends Animation{
       this.totalDistance += distance;
       this.distancePerSegment.push(distance);
     }
-
-    // Calcula the speed
+    // Calculate the speed
     this.speed = this.totalDistance / this.time;
-
     // Calculate the time between conscutive control points
     for (var i = 0; i < this.distancePerSegment.length; i++) {
       this.timePerSegment.push(this.distancePerSegment[i] / this.speed);
     }
+
+    this.updateMovementDirection();
+  }
+
+
+  /**
+  * Update current movement direction (starting and final points)
+  * and the directional angle
+  */
+  updateMovementDirection() {
+    this.xi = this.controlPoints[this.index][0];
+    this.yi = this.controlPoints[this.index][1];
+    this.zi = this.controlPoints[this.index][2];
+    this.xf = this.controlPoints[this.index + 1][0];
+    this.yf = this.controlPoints[this.index + 1][1];
+    this.zf = this.controlPoints[this.index + 1][2];
+    this.angle = Math.atan2(this.zf - this.zi, this.xf - this.xi);
+  }
+
+  update(time) {
+    
+
+  }
+
+  apply() {
+
   }
 }
