9e6feb0f50febac9074152f1474be6aac3deb141
Arthur26041996 <arthurmatta@hotmail.com>
2018-11-25 17:32:24 +0000

    finished MyPlane

--- TP1/tp1/Primitives/MyPlane.js
+++ TP1/tp1/Primitives/MyPlane.js
@@ -1,50 +1,15 @@
 function Plane(scene, parts) {
 	this.scene = scene;
-	CGFobject.call(this,scene);
+	CGFobject.call(this, scene);
 
+	this.npartsU = parts.npartsU;
+	this.npartsV = parts.npartsV;
 
+	this.buildControPoints(this.npartsU, this.npartsV);
 
-
-
-
-	var controlPoints = [
-							[
-								[0.5, 0, -0.5, 1],
-								[0.5, 0, 0.5, 1]
-							],
-							[
-								[-0.5, 0, -0.5, 1],
-								[-0.5, 0, 0.5, 1]
-							]
-						];
-
-	getSurfacePoint = function(u, v) {
-		return nurbsSurface.getPoint(u, v);
-	};
-
-  this.plane =this.makeSurface(2, // degree on U: 3 control vertexes U
-						 1, // degree on V: 2 control vertexes on V
-						[	// U = 0
-							[ // V = 0..1;
-								 [ -1.5, -1.5, 0.0, 1 ],
-								 [ -1.5,  1.5, 0.0, 1 ]
-
-							],
-							// U = 1
-							[ // V = 0..1
-								 [ 0, -1.5, 3.0, 1 ],
-								 [ 0,  1.5, 3.0, 1 ]
-							],
-							// U = 2
-							[ // V = 0..1
-								[ 1.5, -1.5, 0.0, 1 ],
-								[ 1.5,  1.5, 0.0, 1 ]
-							]
-						]);
-	//this.makeSurface(1,1,controlPoints);//new CGFnurbsObject(this.scene,40,80,nurbsSurface);
-
-
-}
+	// console.log(this.controlPoints);
+  	this.plane = this.makeSurface( this.npartsU, this.npartsV, this.controlPoints);
+};
 
 Plane.prototype = Object.create(CGFobject.prototype);
 Plane.prototype.constructor = Plane;
@@ -53,20 +18,34 @@ Plane.prototype.constructor = Plane;
 
 Plane.prototype.display = function() {
 
-		this.scene.pushMatrix();
-	  this.plane.display();
-    //CGFnurbsObject.prototype.display.call(this.plane);
-		this.scene.popMatrix();
-}
+	this.scene.pushMatrix();
+	this.plane.display();
+	this.scene.popMatrix();
+};
 
 Plane.prototype.updateTexCoords = function (aS, aT) {}
 
+Plane.prototype.buildControPoints = function(nU, nV) {
+	var matrix = [];
+	const step_v = 1.0 / nV;
+	const step_u = 1.0 / nU;
+	this.controlPoints = [];
 
-Plane.prototype.makeSurface=function(degree1, degree2,c) {
+	for (let value_u = 0; value_u <= 1.0; value_u+=step_u) {
+		for (let value_v = 0; value_v <= 1.0; value_v+=step_v) {
+			matrix.push([-0.5 + value_u, 0.0, 0.5 - value_v, 1.0]);
+		}
+		this.controlPoints.push(matrix);
+		matrix = [];
+	}
+}
 
-		var nurbsSurface = new CGFnurbsSurface(degree1, degree2, c);
 
-		var obj = new CGFnurbsObject(this.scene, 30, 30, nurbsSurface ); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
+Plane.prototype.makeSurface = function(degree1, degree2, controlVertex) {
+
+		var nurbsSurface = new CGFnurbsSurface(degree1, degree2, controlVertex);
+
+		var obj = new CGFnurbsObject(this.scene, 20, 20, nurbsSurface);
 
 		return obj;
-	};
+};
