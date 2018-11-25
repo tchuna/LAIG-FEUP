f2c0af864c0f21d0ab8f4179f772534f86ecb6d1
tchuna <elgneramos@gmail.com>
2018-11-25 02:42:36 +0000

    veicle

--- TP1/tp1/Primitives/Vehicle.js
+++ TP1/tp1/Primitives/Vehicle.js
@@ -1,4 +1,7 @@
 
+var degToRad = Math.PI / 180.0;
+
+
 function Vehicle(scene){
   CGFobject.call(this,scene);
 
@@ -40,7 +43,33 @@ function Vehicle(scene){
     controlPoints: points
   };
 
+   cylnd={
+    base:1,
+    top:1,
+    height: 1,
+    slices:20,
+    stacks: 20
+  };
+
+  to = {
+    inner:1,
+    outer: 1,
+    slices: 20,
+    loops:20
+  };
+
+  sphere= {
+    radius: 1.5,
+    slices: 20,
+    stacks:20
+  };
+
   this.vehicle=new Patch(this.scene,primitive);
+  this.wire=new MyCylinder(this.scene,cylnd);
+  this.base=new MySphere(this.scene,sphere);
+  this.base_1=new MyTorus(this.scene,to);
+
+
 
 }
 
@@ -49,8 +78,57 @@ Vehicle.prototype.constructor=Vehicle;
 
 Vehicle.prototype.display=function(){
   this.scene.pushMatrix();
-  this.vehicle.display();
+	this.scene.translate(0, 3.0, 0);
+	this.vehicle.display();
+	this.scene.popMatrix();
+  this.scene.pushMatrix();
+
+  this.scene.translate(0, 3, -4.5);
+  this.scene.scale(1.35, 1.35, 0.65);
+  this.base_1.display();
+
   this.scene.popMatrix();
+
+
+
+	this.scene.pushMatrix();
+		this.scene.translate(-2.43, 3.9, -4.5);
+		this.scene.rotate(15 * degToRad, 1, 0, 0);
+		this.scene.rotate(20 * degToRad, 0, 0, 1);
+		this.scene.rotate(-Math.PI/8, 1, 0, 0);
+		this.scene.scale(0.02, 0.02, 5);
+		this.wire.display();
+	this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+		this.scene.translate(2.3, 3.9, -4.5);
+		this.scene.rotate(15 * degToRad, 1, 0, 0);
+		this.scene.rotate(-20 * degToRad, 0, 0, 1);
+		this.scene.rotate(-Math.PI/8, 1, 0, 0);
+		this.scene.scale(0.02, 0.02, 5);
+		this.wire.display();
+	this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+		this.scene.translate(-2.43, 1.9, -4.5);
+		this.scene.rotate(-15 * degToRad, 1, 0, 0);
+		this.scene.rotate(-20 * degToRad, 0, 0, 1);
+		this.scene.rotate(-Math.PI/-8, 1, 0, 0);
+		this.scene.scale(0.02, 0.02, 5);
+		this.wire.display();
+	this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+		this.scene.translate(2.3, 1.9, -4.5);
+		this.scene.rotate(-15 * degToRad, 1, 0, 0);
+		this.scene.rotate(20 * degToRad, 0, 0, 1);
+		this.scene.rotate(-Math.PI/-8, 1, 0, 0);
+		this.scene.scale(0.02, 0.02, 5);
+		this.wire.display();
+
+	this.scene.popMatrix();
+
+
 
 };
 
