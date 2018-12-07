6b0a9ec7f6358edb2a8820c0fc9dfe2cd2cec839
Arthur26041996 <arthurmatta@hotmail.com>
2018-12-07 13:31:30 +0000

    Initial implementation of board cell

--- TP3/tp3/Board/BoardCell.js
+++ TP3/tp3/Board/BoardCell.js
@@ -1,3 +1,5 @@
+DEGREE_TO_RAD = Math.PI/180;
+
 function BoardCell(scene){
     CGFobject.call(this, scene);
     this.scene = scene;
@@ -22,6 +24,13 @@ function BoardCell(scene){
         x2: 0.5,
         y2: 0.5
     });
+
+    this.cellMaterial = new CGFappearance(this.scene);
+    this.cellMaterial.setAmbient(1.0, 1.0, 1.0, 1.0);
+    this.cellMaterial.setDiffuse(1.0, 1.0, 1.0, 1.0);
+    this.cellMaterial.setSpecular(1.0, 1.0, 1.0, 1.0);
+    this.cellMaterial.setShininess(10.0);
+
 };
 
 BoardCell.prototype = Object.create(CGFobject.prototype);
@@ -29,9 +38,16 @@ BoardCell.prototype.constructor = BoardCell;
 
 BoardCell.prototype.display = function(){
     this.scene.pushMatrix();
-
+    this.scene.translate(0.0, 0.1, 0.0);
+    this.scene.rotate(-90 * DEGREE_TO_RAD, 1, 0, 0);
     this.circle.display();
-    this.cell.display();
-
     this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+    this.scene.scale(3.0, 1.0, 3.0);
+    this.scene.rotate(-90 * DEGREE_TO_RAD, 1, 0, 0);
+    this.cellMaterial.apply();
+    this.cell.display();
+    this.scene.popMatrix();
+
 };
